d947df85cadbb61ccd571f7860db8d4227081259
Michiel de Jong <michiel@unhosted.org>
2013-11-13 10:37:58 -0800

    load in-page.js

--- src/jetpack/lib/main.js
+++ src/jetpack/lib/main.js
@@ -1,11 +1,14 @@
 var widgets = require("sdk/widget");
 var tabs = require("sdk/tabs");
+var self = require("sdk/self");
  
 var widget = widgets.Widget({
   id: "remotestorage-link",
   label: "remotestorage website",
   contentURL: "http://remotestorage.io/favicon.ico",
   onClick: function() {
-    tabs.open("http://remotestorage.io/");
+    tabs.activeTab.attach({
+      contentScriptFile: self.data.url('in-page.js') 
+    })
   }
 });
