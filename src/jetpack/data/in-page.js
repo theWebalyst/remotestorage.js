b691d44efe3a846aac88fec4c369e076b1524896
Michiel de Jong <michiel@unhosted.org>
2013-11-13 10:47:44 -0800

    use simple-storage https://addons.mozilla.org/en-US/developers/docs/sdk/latest/modules/sdk/simple-storage.html

--- src/jetpack/data/in-page.js
+++ src/jetpack/data/in-page.js
@@ -1,1 +1,5 @@
-document.body.style.border = "5px solid red";
+// "self" is a global object in content scripts
+// Listen for a "drawBorder"
+self.port.on("drawBorder", function(color) {
+  document.body.style.border = "5px solid " + color;
+});
--- src/jetpack/lib/main.js
+++ src/jetpack/lib/main.js
@@ -1,14 +1,17 @@
 var widgets = require("sdk/widget");
 var tabs = require("sdk/tabs");
 var self = require("sdk/self");
+var ss = require("sdk/simple-storage");
  
 var widget = widgets.Widget({
   id: "remotestorage-link",
   label: "remotestorage website",
   contentURL: "http://remotestorage.io/favicon.ico",
   onClick: function() {
-    tabs.activeTab.attach({
+    worker = tabs.activeTab.attach({
       contentScriptFile: self.data.url('in-page.js') 
-    })
+    });
+    ss.storage.color = 'red';
+    worker.port.emit('drawBorder', ss.storage.color);
   }
 });
