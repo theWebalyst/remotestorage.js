0384e1155220a07c1dc89577f2d211ce4d368184
Michiel de Jong <michiel@unhosted.org>
2013-11-13 11:30:20 -0800

    play with postMessage between page script and content script https://addons.mozilla.org/en-US/developers/docs/sdk/latest/dev-guide/guides/content-scripts/communicating-with-other-scripts.html

--- src/jetpack/data/in-page.js
+++ src/jetpack/data/in-page.js
@@ -2,4 +2,21 @@
 // Listen for a "drawBorder"
 self.port.on("drawBorder", function(color) {
   document.body.style.border = "5px solid " + color;
+  document.defaultView.postMessage("Message from content script", "http://example.com/");
+  console.log('abcd');
 });
+self.port.on('callback', function(cb, key) {
+   cb(key);
+});
+document.defaultView.addEventListener('message', function(event) {
+  console.log(event.data);
+  console.log(event.origin);
+}, false);
+/*{
+  setItem: function(key, val) {
+    self.port.emit('setItem', key, val);
+  },
+  getItem: function(key, cb) {
+    self.port.emit('getItem', key, cb);
+  }
+};*/
